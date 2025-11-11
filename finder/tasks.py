0535a0216afe8c9b3323c94fdeec266a49a81dd7
Partner <partners@first4figures.com>
2025-11-12 08:44:41 +0900

    Run celery via Redis

--- finder/tasks.py
+++ finder/tasks.py
@@ -1,14 +1,12 @@
+import os
 from celery import Celery
-from dotenv import load_dotenv
-import os, requests, json
 
-load_dotenv()
-REDIS_URL = os.getenv("REDIS_URL")
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
-celery_app = Celery("finder", broker=REDIS_URL, backend=REDIS_URL)
-celery_app.conf.update(task_serializer="json", result_serializer="json")
+app = Celery("finder", broker=redis_url, backend=redis_url)
+app.conf.update(task_serializer="json", result_serializer="json")
 
-@celery_app.task
+@app.task
 def validate_domain(domain):
     try:
         r = requests.get(f"https://{domain}", timeout=5)
